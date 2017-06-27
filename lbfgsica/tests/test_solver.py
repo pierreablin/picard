8c1ebe39c1b2f60ab984dcf7cb246ff713569e52
Pierre Ablin <pierreablin@gmail.com>
2017-06-27 03:01:53 +0200

    updated tests

--- lbfgsica/tests/test_solver.py
+++ lbfgsica/tests/test_solver.py
@@ -16,11 +16,3 @@ def test_lbgfs_h2_solver():
     A = np.random.randn(N, N)
     X = np.dot(A, S)
     Y, W = lbfgs_ica(X, precon=2)
-
-
-def test_lbfgs_crash():
-    N, T = 15, 10000
-    S = np.random.laplace(size=(N, T))
-    A = np.random.randn(N, N)
-    X = np.dot(A, S)
-    Y, W = lbfgs_ica(X, precon=42)
