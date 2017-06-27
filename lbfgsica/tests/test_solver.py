b76926ea72daf0467e98d145f21dfdc1dd83f62b
Pierre Ablin <pierreablin@gmail.com>
2017-06-27 02:25:11 +0200

    more exhastive tests for codecov

--- lbfgsica/tests/test_solver.py
+++ lbfgsica/tests/test_solver.py
@@ -3,12 +3,24 @@ from lbfgsica import lbfgs_ica
 
 
 def test_lbgfs_solver():
-    N, T = 2, 1000
+    N, T = 15, 10000
     S = np.random.laplace(size=(N, T))
     A = np.random.randn(N, N)
     X = np.dot(A, S)
-    lbfgs_ica(X, verbose=True)
+    Y, W = lbfgs_ica(X, precon=2)
 
 
-if __name__ == '__main__':
-    test_lbgfs_solver()
+def test_lbgfs_h2_solver():
+    N, T = 15, 10000
+    S = np.random.laplace(size=(N, T))
+    A = np.random.randn(N, N)
+    X = np.dot(A, S)
+    Y, W = lbfgs_ica(X, precon=2)
+
+
+def test_lbfgs_crash():
+    N, T = 15, 10000
+    S = np.random.laplace(size=(N, T))
+    A = np.random.randn(N, N)
+    X = np.dot(A, S)
+    Y, W = lbfgs_ica(X, precon=42)
