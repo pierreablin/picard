b01a9f4cac94a4dc425774363df86f430273d4da
Pierre Ablin <pierreablin@gmail.com>
2017-06-27 03:18:48 +0200

    updated tests

--- lbfgsica/tests/test_solver.py
+++ lbfgsica/tests/test_solver.py
@@ -3,7 +3,7 @@ from lbfgsica import lbfgs_ica
 
 
 def test_lbgfs_solver():
-    N, T = 15, 10000
+    N, T = 2, 1000
     S = np.random.laplace(size=(N, T))
     A = np.random.randn(N, N)
     X = np.dot(A, S)
@@ -11,8 +11,19 @@ def test_lbgfs_solver():
 
 
 def test_lbgfs_h2_solver():
-    N, T = 15, 10000
+    N, T = 2, 1000
     S = np.random.laplace(size=(N, T))
     A = np.random.randn(N, N)
     X = np.dot(A, S)
     Y, W = lbfgs_ica(X, precon=2)
+
+
+def test_lbgfs_crash():
+    N, T = 2, 1000
+    S = np.random.laplace(size=(N, T))
+    A = np.random.randn(N, N)
+    X = np.dot(A, S)
+    try:
+        Y, W = lbfgs_ica(X, precon=18)
+    except:
+        pass
