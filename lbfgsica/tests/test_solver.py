ea10f6e21f3fd8bb75cc54171d66ff4388b2fdb0
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-06-28 19:22:23 +0200

    nicer tests

--- lbfgsica/tests/test_solver.py
+++ lbfgsica/tests/test_solver.py
@@ -1,29 +1,30 @@
 import numpy as np
+from nose.tools import assert_raises, assert_equal
+from numpy.testing import assert_allclose
+
 from lbfgsica import lbfgs_ica
 
 
 def test_lbgfs_solver():
-    N, T = 15, 5000
-    S = np.random.laplace(size=(N, T))
-    A = np.random.randn(N, N)
+    N, T = 2, 10000
+    rng = np.random.RandomState(42)
+    S = rng.laplace(size=(N, T))
+    A = rng.randn(N, N)
     X = np.dot(A, S)
-    Y, W = lbfgs_ica(X, verbose=True)
 
-
-def test_lbgfs_h2_solver():
-    N, T = 2, 1000
-    S = np.random.laplace(size=(N, T))
-    A = np.random.randn(N, N)
-    X = np.dot(A, S)
-    Y, W = lbfgs_ica(X, precon=2)
+    for precon in [1, 2]:
+        Y, W = lbfgs_ica(X, precon=2, verbose=True)
+        assert_equal(Y.shape, X.shape)
+        assert_equal(W.shape, A.shape)
+        WA = np.abs(np.dot(W, A))
+        WA /= np.max(WA, axis=1, keepdims=True)
+        assert_allclose(WA, np.eye(N), rtol=1e-2, atol=1e-2)
 
 
 def test_lbgfs_crash():
     N, T = 2, 1000
-    S = np.random.laplace(size=(N, T))
-    A = np.random.randn(N, N)
+    rng = np.random.RandomState(42)
+    S = rng.laplace(size=(N, T))
+    A = rng.randn(N, N)
     X = np.dot(A, S)
-    try:
-        Y, W = lbfgs_ica(X, precon=18)
-    except:
-        pass
+    assert_raises(ValueError, lbfgs_ica, X, precon=18)
