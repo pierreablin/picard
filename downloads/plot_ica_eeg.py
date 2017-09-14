3c10e85c5be51c66a7a5e5e82f483568de7e1bf5
Pierre Ablin <pierreablin@gmail.com>
2017-09-14 22:02:31 +0200

    init

---
+++ downloads/plot_ica_eeg.py
@@ -0,0 +1,63 @@
+"""
+=======================================================
+Blind source separation using preconditioned ICA on EEG
+=======================================================
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import mne
+from mne.datasets import sample
+from sklearn.decomposition import PCA
+
+from picard import picard
+
+print(__doc__)
+
+###############################################################################
+# Generate sample EEG data
+data_path = sample.data_path()
+raw_fname = data_path + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+
+raw = mne.io.read_raw_fif(raw_fname, preload=True)
+raw.filter(1, 40, n_jobs=1)  # 1Hz high pass is often helpful for fitting ICA
+
+picks = mne.pick_types(raw.info, meg=False, eeg=True, eog=False,
+                       stim=False, exclude='bads')
+
+random_state = 0
+data = raw[picks, :][0]
+data = data[:, ::2]  # decimate a bit
+
+# Center
+data -= np.mean(data, axis=1, keepdims=True)
+
+# Apply PCA for dimension reduction and whitenning.
+
+n_components = 30
+pca = PCA(n_components=n_components, whiten=True, svd_solver='full')
+pca.fit(data)
+
+X = pca.components_ * np.sqrt(data.shape[1])
+
+# Run ICA on X
+
+Y, W = picard(X)
+
+###############################################################################
+# Plot results
+n_plots = 10
+models = [data[:n_plots], Y[:n_plots]]
+names = ['Observations (raw EEG)',
+         'ICA recovered sources']
+fig, axes = plt.subplots(2, 1, figsize=(7, 7))
+for ii, (model, name, ax) in enumerate(zip(models, names, axes)):
+    ax.set_title(name)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    offsets = np.max(model, axis=1) - np.min(model, axis=1)
+    offsets = np.cumsum(offsets)
+    ax.plot((model[:, :1000] + offsets[:, np.newaxis]).T, 'k')
+
+fig.tight_layout()
+plt.show()
---
+++ downloads/plot_ica_synth.py
@@ -0,0 +1,49 @@
+"""
+====================================
+Blind source separation using Picard
+====================================
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from picard import picard
+
+print(__doc__)
+
+###############################################################################
+# Generate sample data
+np.random.seed(0)
+n_samples = 2000
+time = np.linspace(0, 8, n_samples)
+
+s1 = np.sin(2 * time) * np.sin(40 * time)
+s2 = np.sin(3 * time) ** 5
+s3 = np.random.laplace(size=s1.shape)
+
+S = np.c_[s1, s2, s3].T
+
+S /= S.std(axis=1)[:, np.newaxis]  # Standardize data
+# Mix data
+A = np.array([[1, 1, 1], [0.5, 2, 1.0], [1.5, 1.0, 2.0]])  # Mixing matrix
+X = np.dot(A, S)  # Generate observations
+
+# Compute ICA
+Y, W = picard(X)
+
+###############################################################################
+# Plot results
+
+models = [X, S, Y]
+names = ['Observations (mixed signal)',
+         'True Sources',
+         'ICA recovered signals']
+colors = ['red', 'steelblue', 'orange']
+
+for ii, (model, name) in enumerate(zip(models, names), 1):
+    fig, axes = plt.subplots(3, 1, figsize=(6, 4), sharex=True, sharey=True)
+    plt.suptitle(name)
+    for ax, sig, color in zip(axes, model, colors):
+        ax.plot(sig, color=color)
+
+plt.show()
