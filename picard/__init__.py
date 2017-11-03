8d2887e603ff547ed0cfd386984072023db88837
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2017-11-03 11:43:00 +0100

    bump version

--- picard/__init__.py
+++ picard/__init__.py
@@ -16,7 +16,7 @@
 # Dev branch marker is: 'X.Y.devN' where N is an integer.
 #
 
-__version__ = '0.1'
+__version__ = '0.2.dev0'
 
 from .solver import picard  # noqa
 from ._picardo import picardo  # noqa
