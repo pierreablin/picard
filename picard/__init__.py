1599fcfea0e82a925ec25cc2703d65f48335e881
Pierre Ablin <pierreablin@gmail.com>
2018-02-23 18:21:37 +0100

    changed version number

--- picard/__init__.py
+++ picard/__init__.py
@@ -16,7 +16,7 @@
 # Dev branch marker is: 'X.Y.devN' where N is an integer.
 #
 
-__version__ = '0.2.dev1'
+__version__ = '0.3.dev0'
 
 from .solver import picard  # noqa
 from .densities import density
