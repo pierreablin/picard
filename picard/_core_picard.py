fixed hessians

picard/_core_picard.py
@@ -87,7 +87,6 @@ def core_picard(X, density=Tanh(), ortho=False, extended=False, m=7,
         del psiY
         # Compute the squared signals
         Y_square = Y ** 2
-
         # Compute the kurtosis and update the gradient accordingly
         if extended:
             K = np.mean(psidY, axis=1)
@@ -105,6 +104,16 @@ def core_picard(X, density=Tanh(), ortho=False, extended=False, m=7,
             h_off = np.diag(G).copy()
         else:
             h_off = np.ones(N)
+        # Compute the Hessian approximation diagonal and regularize
+        if ortho:
+            psidY_mean = np.mean(psidY, axis=1)
+            diag = psidY_mean[:, None] * np.ones(N)[None, :]
+            h = 0.5 * (diag + diag.T - h_off[:, None] - h_off[None, :])
+            h[h < lambda_min] = lambda_min
+        else:
+            h = np.inner(psidY, Y_square) / T
+            h = _regularize_hessian(h, h_off, lambda_min)
+        del psidY, Y_square
         # Project the gradient if ortho
         if ortho:
             G = (G - G.T) / 2
@@ -130,10 +139,6 @@ def core_picard(X, density=Tanh(), ortho=False, extended=False, m=7,
         if extended and sign_change:
             current_loss = None
             s_list, y_list, r_list = [], [], []
-        # Compute the Hessian approximation diagonal and regularize
-        h = np.inner(psidY, Y_square) / T
-        del psidY, Y_square
-        h = _regularize_hessian(h, h_off, lambda_min)
         # Find the L-BFGS direction
         direction = _l_bfgs_direction(G, h, h_off, s_list, y_list, r_list,
                                       ortho)
@@ -153,7 +158,8 @@ def core_picard(X, density=Tanh(), ortho=False, extended=False, m=7,
         if verbose:
             print('iteration %d, gradient norm = %.4g' %
                   (n + 1, gradient_norm))
-    infos = dict(converged=requested_tolerance, gradient_norm=gradient_norm)
+    infos = dict(converged=requested_tolerance, gradient_norm=gradient_norm,
+                 n_iterations=n)
     return Y, W, infos
 
 
@@ -201,9 +207,11 @@ def _l_bfgs_direction(G, h, h_off, s_list, y_list, r_list, ortho):
         alpha = r * np.sum(s * q)
         a_list.append(alpha)
         q -= alpha * y
-    z = _solve_hessian(h, h_off, q)
     if ortho:
+        z = q / h
         z = (z - z.T) / 2.
+    else:
+        z = _solve_hessian(h, h_off, q)
     for s, y, r, alpha in zip(s_list, y_list, r_list, reversed(a_list)):
         beta = r * np.sum(y * z)
         z += (alpha - beta) * s

picard/solver.py
@@ -14,9 +14,10 @@ from .densities import Tanh, Exp, Cube, check_density
 
 
 def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
-           return_X_mean=False, max_iter=100, tol=1e-07, m=7, ls_tries=10,
-           lambda_min=0.01, check_fun=True, w_init=None, fastica_it=None,
-           random_state=None, verbose=False):
+           return_X_mean=False, return_n_iter=False, centering=True,
+           max_iter=100, tol=1e-07, m=7,  ls_tries=10, lambda_min=0.01,
+           check_fun=True, w_init=None, fastica_it=None, random_state=None,
+           verbose=False):
     """Perform Independent Component Analysis.
 
     Parameters
@@ -49,7 +50,13 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
         In this case the parameter n_components will be ignored.
 
     return_X_mean : bool, optional
-        If True, X_mean is returned too.
+        If True, X_mean is returned too. Equals to 0 if centering is False.
+
+    return_n_iter : bool, option
+        Whether or not to return the number of iterations.
+
+    centering : bool, optional
+        If True, X is mean corrected.
 
     max_iter : int, optional
         Maximum number of iterations to perform.
@@ -76,16 +83,16 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
         Initial un-mixing array of dimension (n.comp,n.comp).
         If None (default) then a random rotation is used.
 
+    fastica_it : int or None, optional (default=None)
+        If an int, perform `fastica_it` iterations of FastICA before running
+        Picard. It might help starting from a better point.
+
     random_state : int, RandomState instance or None, optional (default=None)
         Used to perform a random initialization when w_init is not provided.
         If int, random_state is the seed used by the random number generator;
         If RandomState instance, random_state is the random number generator;
         If None, the random number generator is the RandomState instance used
         by `np.random`.
-
-    fastica_it : int or None, optional (default=None)
-        If an int, perform `fastica_it` iterations of FastICA before running
-        Picard. It might help starting from a better point.
 
     verbose : bool, optional
         Prints informations about the state of the algorithm if True.
@@ -109,6 +116,10 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
 
     X_mean : array, shape (n_features,)
         The mean over features. Returned only if return_X_mean is True.
+
+    n_iter : int
+        Number of iterations taken to converge. This is
+        returned only when return_n_iter is set to `True`.
     """
     random_state = check_random_state(random_state)
     if not type(ortho) is bool:
@@ -132,21 +143,22 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
     if n_components is None:
         n_components = min(n, p)
 
-    # Centering the columns (ie the variables)
-    X_mean = X.mean(axis=-1)
-    X -= X_mean[:, np.newaxis]
+    X1 = X.copy()
+    if centering:
+        # Center the columns (ie the variables)
+        X_mean = X1.mean(axis=-1)
+        X1 -= X_mean[:, np.newaxis]
     if whiten:
         # Whitening and preprocessing by PCA
-        u, d, _ = linalg.svd(X, full_matrices=False)
-
+        u, d, _ = linalg.svd(X1, full_matrices=False)
         del _
         K = (u / d).T[:n_components]
         del u, d
         K *= np.sqrt(p)
-        X1 = np.dot(K, X)
+        X1 = np.dot(K, X1)
     else:
         # X must be casted to floats to avoid typing issues with numpy 2.0
-        X1 = X.astype('float')
+        X1 = X1.astype('float')
 
     # Initialize
     if w_init is None:
@@ -164,7 +176,6 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
         w_init = _ica_par(X1, fun, fastica_it, w_init, verbose)
 
     X1 = np.dot(w_init, X1)
-
     if ortho:
         extended = True
     else:
@@ -184,7 +195,20 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
                       % (gradient_norm, tol))
     if not whiten:
         K = None
+    n_iter = infos['n_iterations']
     if return_X_mean:
-        return K, W, Y, X_mean
+        if centering:
+            if return_n_iter:
+                return K, W, Y, X_mean, n_iter
+            else:
+                return K, W, Y, X_mean
+        else:
+            if return_n_iter:
+                return K, W, Y, np.zeros(p), n_iter
+            else:
+                return K, W, Y, np.zeros(p)
     else:
-        return K, W, Y
+        if return_n_iter:
+            return K, W, Y, n_iter
+        else:
+            return K, W, Y

picard/tests/test_solver.py
@@ -20,13 +20,13 @@ def test_dimension_reduction():
     S = rng.laplace(size=(N, T))
     A = rng.randn(N, N)
     X = np.dot(A, S)
-    K, W, Y = picard(X.copy(), n_components=n_components, ortho=False,
+    K, W, Y = picard(X, n_components=n_components, ortho=False,
                      random_state=rng, max_iter=2)
     assert_equal(K.shape, (n_components, N))
     assert_equal(W.shape, (n_components, n_components))
     assert_equal(Y.shape, (n_components, T))
     with warnings.catch_warnings(record=True) as w:
-        K, W, Y = picard(X.copy(), n_components=n_components, ortho=False,
+        K, W, Y = picard(X, n_components=n_components, ortho=False,
                          whiten=False, max_iter=1)
         assert len(w) == 2
 
@@ -48,7 +48,7 @@ def test_dots():
             else:
                 w_init = np.eye(N)
         with warnings.catch_warnings(record=True):
-            K, W, Y, X_mean = picard(X.copy(), ortho=ortho, whiten=whiten,
+            K, W, Y, X_mean = picard(X, ortho=ortho, whiten=whiten,
                                      return_X_mean=True, w_init=w_init,
                                      n_components=n_component,
                                      random_state=rng, max_iter=2,
@@ -72,7 +72,7 @@ def test_pre_fastica():
             S = rng.uniform(low=-1, high=1, size=(N, T))
         A = rng.randn(N, N)
         X = np.dot(A, S)
-        K, W, Y = picard(X.copy(), fun=fun, ortho=False, random_state=0,
+        K, W, Y = picard(X, fun=fun, ortho=False, random_state=0,
                          fastica_it=10)
         if fun == 'tanh':
             fun = Tanh()
@@ -107,7 +107,7 @@ def test_picard():
             S = rng.uniform(low=-1, high=1, size=(N, T))
         A = rng.randn(N, N)
         X = np.dot(A, S)
-        K, W, Y = picard(X.copy(), fun=fun, ortho=False, random_state=0)
+        K, W, Y = picard(X, fun=fun, ortho=False, random_state=0)
         if fun == 'tanh':
             fun = Tanh()
         elif fun == 'exp':
@@ -137,12 +137,16 @@ def test_shift():
     A = rng.randn(N, N)
     offset = rng.randn(N)
     X = np.dot(A, S) + offset[:, None]
-    _, W, Y, X_mean = picard(X.copy(), ortho=False, whiten=False,
+    _, W, Y, X_mean = picard(X, ortho=False, whiten=False,
                              return_X_mean=True, random_state=rng)
     assert_allclose(offset, X_mean, rtol=0, atol=0.2)
     WA = W.dot(A)
     WA = permute(WA)
     assert_allclose(WA, np.eye(N), rtol=0, atol=0.2)
+    _, W, Y, X_mean = picard(X, ortho=False, whiten=False,
+                             centering=False,  return_X_mean=True,
+                             random_state=rng)
+    assert_allclose(X_mean, 0)
 
 
 def test_picardo():
@@ -155,7 +159,7 @@ def test_picardo():
     for fastica_it in [None, 2]:
         for fun in names:
             print(fun)
-            K, W, Y = picard(X.copy(), fun=fun, ortho=True, random_state=rng,
+            K, W, Y = picard(X, fun=fun, ortho=True, random_state=rng,
                              fastica_it=fastica_it, verbose=True)
             if fun == 'tanh':
                 fun = Tanh()
@@ -202,3 +206,37 @@ def test_bad_custom_density():
 def test_fun():
     for fun in [Tanh(), Exp(), Cube()]:
         check_density(fun)
+
+
+def test_no_regression():
+    n_tests = 10
+    baseline = {}
+    baseline['lap', True] = 17.
+    baseline['lap', False] = 23.
+    baseline['gauss', True] = 52.
+    baseline['gauss', False] = 60.
+    N, T = 10, 1000
+    for mode in ['lap', 'gauss']:
+        for ortho in [True, False]:
+            n_iters = []
+            for i in range(n_tests):
+                rng = np.random.RandomState(i)
+                if mode == 'lap':
+                    S = rng.laplace(size=(N, T))
+                else:
+                    S = rng.randn(N, T)
+                A = rng.randn(N, N)
+                X = np.dot(A, S)
+                _, _, _, n_iter = picard(X, return_n_iter=True,
+                                         ortho=ortho, random_state=rng)
+                n_iters.append(n_iter)
+            n_mean = np.mean(n_iters)
+            nb_mean = baseline[mode, ortho]
+            err_msg = ('mode=%s, ortho=%s. %d iterations, expecting <%d.' %
+                       (mode, ortho, n_mean, nb_mean))
+            print(err_msg)
+            assert n_mean < nb_mean, err_msg
+
+
+if __name__ == '__main__':
+    test_no_regression()