2b9984378839ffebae7c3db7e2880e7501c26d41
Lukas <l.stranger@student.tugraz.at>
2018-12-03 18:59:38 +0100

    correct mistake in docu

--- picard/solver.py
+++ picard/solver.py
@@ -108,7 +108,7 @@ def picard(X, fun='tanh', n_components=None, ortho=True, whiten=True,
         Estimated un-mixing matrix.
         The mixing matrix can be obtained by::
 
-            w = np.dot(W, K.T)
+            w = np.dot(W, K)
             A = np.dot(w.T, np.linalg.inv(np.dot(w, w.T)))
 
     Y : array, shape (n_components, n_samples) | None
