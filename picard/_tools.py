b1f5088aa2cf770517b5c0f47dd9a282684f5b09
Pierre Ablin <pierreablin@gmail.com>
2020-08-05 11:45:28 +0200

    add amari distance

--- picard/_tools.py
+++ picard/_tools.py
@@ -89,3 +89,11 @@ def _ica_par(X, fun, max_iter, w_init, verbose):
     if verbose:
         print('Running Picard...')
     return W
+
+
+def amari_d(W, A):
+    P = np.dot(W, A)
+
+    def s(r):
+        return np.sum(np.sum(r ** 2, axis=1) / np.max(r ** 2, axis=1) - 1)
+    return (s(np.abs(P)) + s(np.abs(P.T))) / (2 * P.shape[0])
