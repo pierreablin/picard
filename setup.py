c5d968760f5e249e389fd6aa7abf74008bebeac1
Pierre Ablin <pierreablin@gmail.com>
2021-05-24 20:09:35 +0200

    fix name in setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
               'numpy',
               'scipy',
               'numexpr',
-              'sklearn'
+              'scikit-learn'
           ],
           packages=package_tree('picard'),
           )
